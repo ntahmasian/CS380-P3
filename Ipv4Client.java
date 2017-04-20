b1e0d4576a8f2eb2ef32131da9e486d2a2d55dbc
Narvik Tahmasian <Narvik@b98-aruba1-guest-10-110-60-83.wlan.cpp.edu>
2017-04-20 10:39:15 -0700

    Second Commit

--- Ipv4Client.java
+++ Ipv4Client.java
@@ -14,12 +14,63 @@ public class Ipv4Client {
 
             InputStream getIS = socket.getInputStream();
             OutputStream outStream = socket.getOutputStream();
-
-
+            
+            Version(outStream);
+            HLen(outStream);
+            TOS(outStream);
+            Length(outStream);
+            Ident(outStream);
+            Flags(outStream);
+            Offset(outStream);
+            TTL(outStream);
+            Protocol(outStream);
+            Checksum(outStream);
+            SourceAddr(outStream);
+            DestinationAddr(outStream);
+            Data(outStream);
 
         }
         catch(IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void Version(OutputStream outStream) {
+    }
+
+    private static void HLen(OutputStream outStream) {
+    }
+
+    private static void TOS(OutputStream outStream) {
+    }
+
+    private static void Length(OutputStream outStream) {
+    }
+
+    private static void Ident(OutputStream outStream) {
+    }
+
+    private static void Flags(OutputStream outStream) {
+    }
+
+    private static void Offset(OutputStream outStream) {
+    }
+
+    private static void TTL(OutputStream outStream) {
+    }
+
+    private static void Protocol(OutputStream outStream) {
+    }
+
+    private static void Checksum(OutputStream outStream) {
+    }
+
+    private static void SourceAddr(OutputStream outStream) {
+    }
+
+    private static void DestinationAddr(OutputStream outStream) {
+    }
+
+    private static void Data(OutputStream outStream) {
     }
 }
